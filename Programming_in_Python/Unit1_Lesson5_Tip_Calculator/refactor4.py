f5ab272e1062bef78915699a6cc9f104cdb68178
cdorman1 <cdorman09@gmail.com>
2014-01-19 22:38:25 -0600

    adding refactor4.py

---
+++ Programming_in_Python/Unit1_Lesson5_Tip_Calculator/refactor4.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+"""
+Created on Fri Jan 03 17:33:32 2014
+
+@author: cdorman
+"""
+from optparse import OptionParser
+
+
+parser = OptionParser()
+
+parser.add_option("-m", "--meal",  dest="first_arg", help="Please provide the cost of your meal without tax and tip",
+                    type="float")
+parser.add_option("-t", "--tax", dest="second_arg", help="Please provide the tax amount as a decimal", 
+                    type="float")
+parser.add_option("-p", "--tip", dest="third_arg", help="Please provide the tip amount as a decimal",
+                    type="float", default=0.15)
+
+(options, args) = parser.parse_args()
+
+
+if not (options.first_arg):
+    parser.error("Please provide the cost of your meal without tax and tip as -m")
+if not (options.second_arg):
+    parser.error("Please provide the tax amount as a decimal -t")
+if not (options.third_arg):
+    parser.error( "Please provide the tip amount as a decimal -p")
+
+
+meal = (options.first_arg) 
+tax = (options.second_arg)
+tip = (options.third_arg)
+
+
+tax_value = meal * tax
+meal_with_tax = meal + meal * tax
+tip_value = meal_with_tax * tip
+total = meal_with_tax + tip_value
+
+
+print "The base cost of your meal was $%.2f" % (meal)
+print "You need to pay $%.2f for tax." % (tax_value)
+print "Tipping at a rate of %.2f%%, you should leave $%.2f for a tip." % (tip, tip_value)
+print "The grand total of your meal is $%.2f." % (total)
