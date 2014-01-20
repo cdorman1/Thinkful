cc8c2d1fa0d1daa14fddb2619b6662cefdba1d27
cdorman1 <cdorman09@gmail.com>
2014-01-20 00:01:25 -0600

    added new code

--- Programming_in_Python/Unit1_Lesson5_Tip_Calculator/refactor4.py
+++ Programming_in_Python/Unit1_Lesson5_Tip_Calculator/refactor4.py
@@ -13,12 +13,12 @@ from optparse import OptionParser
 parser = OptionParser()
 
 parser.add_option("-m", "--meal", dest = "meal", help = "Base cost of meal", type="float")
-parser.add_option("-t", "--tax", dest = "tax", help = "tax_percent", type="float")
-parser.add_option("-p", "--tip", dest = "tip", help = "tip_percent", type = "float", default = 0.15)
+parser.add_option("-t", "--tax", dest = "tax_percent", help = "tax_percent", type="float")
+parser.add_option("-p", "--tip", dest = "tip_percent", help = "percent tip you want to leave", type = "float", default = 0.15)
 
 (options, args) = parser.parse_args()
 
-if not (options.meal and options.tax and options.tip):
+if not (options.meal and options.tax_percent and options.tip_percent):
     parser.error("""
                 Please provide the cost of your meal as -m (i.e. -m 20) without tax or tip.
                 provide the tax amount in decimal form as -t (i.e. -t 0.15).
@@ -27,19 +27,19 @@ if not (options.meal and options.tax and options.tip):
                 """)
 if not (options.meal):
     parser.error("Please provide the cost of your meal without tax and tip as -m")
-if not (options.tax):
+if not (options.tax_percent):
     parser.error("Please provide the tax amount as a decimal -t")
-if not (options.tip):
+if not (options.tip_percent):
     parser.error( "Please provide the tip amount as a decimal -p")
 
 
-tax_value = options.meal * options.tax
-meal_with_tax = options.meal + options.meal * options.tax
-tip_value = meal_with_tax * options.tip
+tax_value = options.meal * options.tax_percent
+meal_with_tax = options.meal + options.meal * options.tax_percent
+tip_value = meal_with_tax * options.tip_percent
 total = meal_with_tax + tip_value
 
 
 print "The base cost of your meal was $%.2f" % (options.meal)
 print "You need to pay $%.2f for tax." % (tax_value)
-print "Tipping at a rate of %.2f%%, you should leave $%.2f for a tip." % (options.tip, tip_value)
+print "Tipping at a rate of %.2f%%, you should leave $%.2f for a tip." % (options.tip_percent, tip_value)
 print "The grand total of your meal is $%.2f." % (total)
