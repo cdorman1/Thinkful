f213619d1e79c4bcb4991cc009cf004689c11e52
cdorman1 <cdorman09@gmail.com>
2014-01-19 23:49:41 -0600

    refactored code

--- Programming_in_Python/Unit1_Lesson5_Tip_Calculator/refactor4.py
+++ Programming_in_Python/Unit1_Lesson5_Tip_Calculator/refactor4.py
@@ -12,42 +12,34 @@ from optparse import OptionParser
 
 parser = OptionParser()
 
-parser.add_option("-m", "--meal",  dest = "first_arg", help = "Please provide the cost of your meal without tax and tip",
-                    type="float")
-parser.add_option("-t", "--tax", dest = "second_arg", help = "Please provide the tax amount as a decimal", 
-                    type="float")
-parser.add_option("-p", "--tip", dest = "third_arg", help = "Please provide the tip amount as a decimal",
-                    type = "float", default = 0.15)
+parser.add_option("-m", "--meal", dest = "meal", help = "Base cost of meal", type="float")
+parser.add_option("-t", "--tax", dest = "tax", help = "tax_percent", type="float")
+parser.add_option("-p", "--tip", dest = "tip", help = "tip_percent", type = "float", default = 0.15)
 
 (options, args) = parser.parse_args()
 
-if not (options.first_arg and options.second_arg and options.third_arg):
+if not (options.meal and options.tax and options.tip):
     parser.error("""
                 Please provide the cost of your meal as -m (i.e. -m 20) without tax or tip.
                 provide the tax amount in decimal form as -t (i.e. -t 0.15).
                 provide the amount you would like to leave for a tip in decimal form as -p. 
                 Note: if there is no value provided for tip a default of 0.15 will be used.
                 """)
-if not (options.first_arg):
+if not (options.meal):
     parser.error("Please provide the cost of your meal without tax and tip as -m")
-if not (options.second_arg):
+if not (options.tax):
     parser.error("Please provide the tax amount as a decimal -t")
-if not (options.third_arg):
+if not (options.tip):
     parser.error( "Please provide the tip amount as a decimal -p")
 
 
-meal = (options.first_arg) 
-tax = (options.second_arg)
-tip = (options.third_arg)
-
-
-tax_value = meal * tax
-meal_with_tax = meal + meal * tax
-tip_value = meal_with_tax * tip
+tax_value = options.meal * options.tax
+meal_with_tax = options.meal + options.meal * options.tax
+tip_value = meal_with_tax * options.tip
 total = meal_with_tax + tip_value
 
 
-print "The base cost of your meal was $%.2f" % (meal)
+print "The base cost of your meal was $%.2f" % (options.meal)
 print "You need to pay $%.2f for tax." % (tax_value)
-print "Tipping at a rate of %.2f%%, you should leave $%.2f for a tip." % (tip, tip_value)
+print "Tipping at a rate of %.2f%%, you should leave $%.2f for a tip." % (options.tip, tip_value)
 print "The grand total of your meal is $%.2f." % (total)
